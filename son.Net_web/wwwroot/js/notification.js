b6719848f8996f7d1486bb16acbae7b3db1d0668
PierreBossard <pbossard.ing2022@esaip.org>
2021-05-12 10:27:35 +0200

    Update

--- son.Net_web/wwwroot/js/notification.js
+++ son.Net_web/wwwroot/js/notification.js
@@ -1,18 +1,28 @@
 "use strict";
 
 const connection = new signalR.HubConnectionBuilder().withUrl("/ringHub").build();
-
+let table = document.getElementById('messagesList');
+let gif = Freezeframe(document.getElementById('bell-gif'))
 
 connection.on("ReceiveNotifications", function (message, list) {
     
-   
     let encodedMsg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     
+    
     for (let i= 0; i < list.length; i++){
+        
         let date = list[i].date;
+        let splitDate = date.split(' ');
+        
+        let row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = i;
+        cell2.innerHTML = splitDate[0];
+        cell3.innerHTML = splitDate[1];
         console.log(date);
     }
-   
     notify(encodedMsg);
 });
 
@@ -23,7 +33,6 @@ const notify = function (message) {
             body : 'Ajouter texte corps',
             silent : false
         };
-        
         //TODO IMPLEMENT ACTION IN NOTIF
         var notif = new Notification(message, options);
         
@@ -37,7 +46,6 @@ connection.start().then(function () {
             .then((permission) => {
                 if(permission !== "granted"){
                     alert('Vous devez autoriser les notifications pour utiliser le service.')
-
                 }
 
             }).catch((error) => {
